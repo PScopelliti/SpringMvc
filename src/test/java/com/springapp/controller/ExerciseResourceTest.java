e2748ee820e01c3b4976314cc55a0283cf14ee73
Giuseppe <peppe.scopelliti@gmail.com>
2015-07-19 16:27:48 +0100

    Fixed exercise unit test

--- src/test/java/com/springapp/controller/ExerciseResourceTest.java
+++ src/test/java/com/springapp/controller/ExerciseResourceTest.java
@@ -1,23 +1,33 @@
 package com.springapp.controller;
 
-import com.springapp.configuration.web.WebMvcConfig;
+import com.springapp.requestmap.RequestMappingBean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ModelMap;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration(classes = {WebMvcConfig.class}, loader = AnnotationConfigContextLoader.class)
 public class ExerciseResourceTest {
 
-    //@InjectMocks
+    private static final String EXERCISE_PAGE_RESULT = "exercise";
+
+    @InjectMocks
+    private RequestMappingBean requestMappingBean;
+
+    @Mock
     private ExerciseResource exerciseResource;
 
     private MockMvc mockMvc;
@@ -25,18 +35,21 @@ public class ExerciseResourceTest {
     @Before
     public void setup() {
         // Process mock annotations
-//        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(this);
 
-        exerciseResource = new ExerciseResourceBean();
         // Setup Spring test in standalone mode
-        this.mockMvc = MockMvcBuilders.standaloneSetup(exerciseResource).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(requestMappingBean).build();
+
+        when(exerciseResource.printWelcome(any(ModelMap.class))).thenReturn(EXERCISE_PAGE_RESULT);
     }
 
     @Test
-    public void testHomePage() throws Exception {
+    public void testExercisesPage() throws Exception {
 
         mockMvc.perform(get("/"))
-                .andExpect(view().name("exercise"));
+                .andExpect(view().name(EXERCISE_PAGE_RESULT));
+
+        verify(exerciseResource, times(1)).printWelcome(any(ModelMap.class));
 
     }
 }
