2f5a95293e252b16e23c18324619e7142fd8b34e
Giuseppe <peppe.scopelliti@gmail.com>
2015-09-13 22:23:36 +0100

    Seems the right direction for security :)

--- src/main/java/com/springapp/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/springapp/configuration/security/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.springapp.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 /**
  * This class contains configuration for security.
@@ -12,7 +14,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
     }
 }
